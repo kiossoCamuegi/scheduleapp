ceff858e64a76ce090147b0a9665a3189c12273b
Kiosso camuegi <72392628+kiossoCamuegi@users.noreply.github.com>
2024-04-18 14:10:59 +0100

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,33 @@
+function ToggleNavbarFilters() {
+    let navbar = document.querySelector(".drop-area");
+    let table_area = document.querySelector(".table-box");
+    let toggle  =  navbar.querySelector(".toggle");
+
+
+      toggle.onclick  = ()=>{
+         navbar.classList.toggle("active");
+        table_area.classList.toggle("mt");
+      }
+}
+ 
+ToggleNavbarFilters();
+
+
+function ToggleTableTask (){
+    let TableItems1 = document.querySelectorAll(".t1");
+    let TableItems2 = document.querySelectorAll(".t2");
+ 
+    for(let i = 0; i < TableItems1.length; i++) {
+         let button = TableItems1[i].querySelector(".ct .drop"); 
+
+       button.addEventListener("click", ()=>{
+        TableItems1[i].classList.toggle("active");
+        TableItems2[i].classList.toggle("active");
+       });
+
+    }
+
+     
+}
+
+ToggleTableTask();
